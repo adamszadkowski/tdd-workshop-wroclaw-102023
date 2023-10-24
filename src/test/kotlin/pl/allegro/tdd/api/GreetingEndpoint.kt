102369adc45df96018a8c7766e2faefe101678d7
Adam Szadkowski <adam.szadkowski@allegro.pl>
2023-10-24 14:18:09 +0200

    Update greeting

--- src/test/kotlin/pl/allegro/tdd/api/GreetingEndpoint.kt
+++ src/test/kotlin/pl/allegro/tdd/api/GreetingEndpoint.kt
@@ -1,17 +1,25 @@
 package pl.allegro.tdd.api
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.atomic.AtomicReference
 
 @RestController
 @RequestMapping("/greeting")
 class GreetingEndpoint {
+    private val message = AtomicReference("hello world")
 
     @GetMapping
-    fun getGreeting(): GreetingResponse = GreetingResponse("hello world")
+    fun getGreeting(): GreetingResponse = GreetingResponse(message.get())
+
+    @PutMapping
+    fun updateGreeting(@RequestBody request: UpdateRequest): GreetingResponse =
+        GreetingResponse(message.updateAndGet { request.message })
 }
 
 data class GreetingResponse(
+    val message: String,
+)
+
+data class UpdateRequest(
     val message: String,
 )
